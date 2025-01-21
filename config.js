7345e4f3974cfbeb2141d66542e56b6c6a4ecf7a
Ahmad20004lll <jksantak2004@gmail.com>
2025-01-21 16:54:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
 global.pairingNumber = process.env.BOT_NUMBER || '923331572777' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'OUQTzZba#RT5yDc54FD0_6ZiiL5nbdrS5Dx22CuLW0RtbCCXuRJs' ;  // put your session id here
 
 global.mods = []
 global.prems = []
