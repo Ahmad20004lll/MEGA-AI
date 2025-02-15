12f5daaa7baf0a5ad6add425b0fdc3a9362f8580
Ahmad20004lll <jksantak2004@gmail.com>
2025-02-15 16:33:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '923321572777';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '923321572777' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'TFYHjBTB#FKRWPoL_emJJqEz3c7mDcEVJtKufWU5JNX3_blBm3ZE' ;  // put your session id here
 
 global.mods = []
 global.prems = []
